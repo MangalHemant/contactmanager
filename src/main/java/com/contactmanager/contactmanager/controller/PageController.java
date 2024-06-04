79577ba6698c2dc5aebb120dc3e488881d100df4
MangalHemant <hemantmangal22@gmail.com>
2024-06-05 02:11:14 +0530

    navbar

--- src/main/java/com/contactmanager/contactmanager/controller/PageController.java
+++ src/main/java/com/contactmanager/contactmanager/controller/PageController.java
@@ -1,30 +1,54 @@
 package com.contactmanager.contactmanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
-@RequestMapping("/home")
-    public String home()
-    {
-    System.out.println("Hello");
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        // sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
+        model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
         return "home";
-
     }
 
-    //about 
-   
-   @RequestMapping("/about")
-    public String aboutPage()
-    {
-        System.out.println("Hello About");
+    // about route
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
         return "about";
-
     }
 
+    // services
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("services page loading");
+        return "services";
+    }
+
+    // contact page
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 }
