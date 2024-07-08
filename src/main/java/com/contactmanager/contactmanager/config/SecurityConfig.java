bb4252e87c9c70a9b5d8175720415f9d7133a90d
MangalHemant <hemantmangal22@gmail.com>
2024-07-09 02:58:38 +0530

    logout validation

--- src/main/java/com/contactmanager/contactmanager/config/SecurityConfig.java
+++ src/main/java/com/contactmanager/contactmanager/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -116,6 +117,12 @@ public class SecurityConfig {
 
             // });
 
+        });
+
+          httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
         });
 
         return httpSecurity.build();
