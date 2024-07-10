c96bb9c1ba37bea3ce181b8bfb06221b0895fc10
MangalHemant <hemantmangal22@gmail.com>
2024-07-10 23:26:01 +0530

    Helper class

---
+++ src/main/java/com/contactmanager/contactmanager/helpers/Helper.java
@@ -0,0 +1,44 @@
+package com.contactmanager.contactmanager.helpers;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class Helper {
+
+    public static String getEmailOfLoggedInUser(Authentication authentication) {
+
+        // Fetching username
+        if (authentication instanceof OAuth2AuthenticationToken) {
+
+            var aOAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+            var clientId = aOAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+
+            var oauth2User = (OAuth2User) authentication.getPrincipal();
+            String username = "";
+
+            if (clientId.equalsIgnoreCase("google")) {
+
+                // sign with google
+                System.out.println("Getting email from google");
+                username = oauth2User.getAttribute("email").toString();
+
+            } else if (clientId.equalsIgnoreCase("github")) {
+
+                // sign with github
+                System.out.println("Getting email from github");
+                username = oauth2User.getAttribute("email") != null ? oauth2User.getAttribute("email").toString()
+                        : oauth2User.getAttribute("login").toString() + "@gmail.com";
+            }
+
+           
+            return username;
+
+        } else {
+            System.out.println("Getting data from local database");
+            return authentication.getName();
+        }
+
+    }
+}
