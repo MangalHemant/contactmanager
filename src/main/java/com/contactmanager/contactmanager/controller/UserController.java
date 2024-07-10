b42e1d9f22a2ac84204e8aae2d727bf81047af72
MangalHemant <hemantmangal22@gmail.com>
2024-07-10 23:25:25 +0530

    user controller changes

--- src/main/java/com/contactmanager/contactmanager/controller/UserController.java
+++ src/main/java/com/contactmanager/contactmanager/controller/UserController.java
@@ -1,12 +1,28 @@
 package com.contactmanager.contactmanager.controller;
 
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+import com.contactmanager.contactmanager.services.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+      private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
 
     // user dashbaord page
 
@@ -19,11 +35,10 @@ public class UserController {
     // user profile page
 
     @RequestMapping(value = "/profile")
-    public String userProfile() {
-        System.out.println("User profile");
+    public String userProfile(Model model, Authentication authentication) {
+
         return "user/profile";
     }
-
     // user add contacts page
 
     // user view contacts
