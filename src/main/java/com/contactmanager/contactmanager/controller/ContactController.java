4969e6c678341c1ff91ad990c18e5e2a69697e85
MangalHemant <hemantmangal22@gmail.com>
2024-07-12 03:38:07 +0530

    contact controller

---
+++ src/main/java/com/contactmanager/contactmanager/controller/ContactController.java
@@ -0,0 +1,97 @@
+package com.contactmanager.contactmanager.controller;
+
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.contactmanager.contactmanager.entities.Contact;
+import com.contactmanager.contactmanager.entities.User;
+import com.contactmanager.contactmanager.forms.ContactForm;
+import com.contactmanager.contactmanager.helpers.Helper;
+import com.contactmanager.contactmanager.helpers.Message;
+import com.contactmanager.contactmanager.helpers.MessageType;
+import com.contactmanager.contactmanager.services.ContactService;
+import com.contactmanager.contactmanager.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/user/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/add")
+    // add contact page: handler
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
+
+        contactForm.setFavorite(true);
+        model.addAttribute("contactForm", contactForm);
+        return "user/add_contact";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result,
+            Authentication authentication, HttpSession session) {
+
+        // process the form data
+
+        // 1 validate form
+
+        // TODO: add validation logic here
+        if (result.hasErrors()) {
+            session.setAttribute("message", Message.builder()
+                    .content("Please correct the following errors")
+                    .type(MessageType.red)
+                    .build());
+            return "user/add_contact";
+        }
+
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+        // form ---> contact
+
+        User user = userService.getUserByEmail(username);
+        // 2 process the contact picture
+        Contact contact = new Contact();
+        contact.setName(contactForm.getName());
+        contact.setFavorite(contactForm.isFavorite());
+        contact.setEmail(contactForm.getEmail());
+        contact.setPhoneNumber(contactForm.getPhoneNumber());
+        contact.setAddress(contactForm.getAddress());
+        contact.setDescription(contactForm.getDescription());
+        contact.setUser(user);
+        contact.setLinkedInLink(contactForm.getLinkedInLink());
+        contact.setWebsiteLink(contactForm.getWebsiteLink());
+        contactService.save(contact);
+        System.out.println(contactForm);
+
+        // 3 set the contact picture url
+
+        // 4 `set message to be displayed on the view
+
+        session.setAttribute("message",
+                Message.builder()
+                        .content("You have successfully added a new contact")
+                        .type(MessageType.green)
+                        .build());
+
+        return "redirect:/user/contacts/add";
+
+    }
+
+}
